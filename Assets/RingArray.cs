47d6ce097876d900ee39382bdcaa316d9e114e95
Steve <steve.tyrell@gmail.com>
2022-06-10 22:22:34 +0100

    add

---
+++ Assets/RingArray.cs
@@ -0,0 +1,20 @@
+// https://answers.unity.com/questions/1661755/how-to-instantiate-objects-in-a-circle-formation-a.html
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingArray : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
